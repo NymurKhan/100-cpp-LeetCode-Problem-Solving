78232915cda059ece6ad583dc5de97e01059d8ec
Nymur Khan Tonmoy <nktonmoy2230@gmail.com>
2024-02-12 10:29:12 +0530

    first

--- sqrtx.cpp
+++ sqrtx.cpp
@@ -1,10 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
 class Solution {
 public:
     int mySqrt(int x) {
         long long low=1,high=x,ans=0;
         while(low<=high){
             long mid=low+(high-low)/2;
-           long long sqr=mid*mid;
+            long long sqr=mid*mid;
             if(sqr==x){
                 return mid;
             }
@@ -19,3 +23,11 @@ public:
         return ans;
     }
 };
+
+int main() {
+    Solution sol;
+    int x = 16; // Example input
+    int result = sol.mySqrt(x);
+    cout << "Square root of " << x << " is: " << result << endl;
+    return 0;
+}
