f125be6bec7e6474ea39b6888a7d98df91eacb02
Nymur Khan Tonmoy <nktonmoy2230@gmail.com>
2024-02-13 13:32:18 +0530

    first

---
+++ deletenode.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node->next;
+        node->val = temp->val;
+        node->next = temp->next;
+        delete temp;
+    }
+};
+
+// Function to print the linked list
+void printList(ListNode* head) {
+    ListNode* temp = head;
+    while (temp != NULL) {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Create the linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    // Delete the node with value 3
+    Solution solution;
+    ListNode* nodeToDelete = head->next->next; // Node with value 3
+    solution.deleteNode(nodeToDelete);
+
+    cout << "List after deleting node with value 3: ";
+    printList(head);
+
+    // Clean up memory
+    ListNode* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
+}
